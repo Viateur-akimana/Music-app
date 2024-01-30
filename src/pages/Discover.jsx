2f00f30066177bb07b6d1a42a4828c73326b007a
Viateur <akimanaviateur94@gmail.com>
2024-01-30 21:17:20 +0200

    started discover page

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,29 @@
-const Discover = () => <div>Discover</div>;
+import React from "react";
+import { genres } from "../assets/constants";
+
+const Discover = () => {
+  const genresTitle = "Pop";
+  return (
+    <div className="flex flex-col">
+      <div className="w-full justify-between items-center sm:flex-row  flex-col m-20">
+        <h2 className="font-bold text-white text-3xl text-left justify-center">
+          Discover {genresTitle}
+        </h2>
+        <select
+          onChange={() => {}}
+          value=""
+          className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none mx-10 mt-3"
+        >
+          {genres.map((genre) => (
+            <option key={genre.value} value={genre.value}>
+              {genre.title}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8"></div>
+    </div>
+  );
+};
 
 export default Discover;
